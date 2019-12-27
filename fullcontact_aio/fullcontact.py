9b66c725b465f1fbe7d74e4eb9069f9fc7628fae
Staging <staging@sublimesecurity.com>
2019-12-27 15:04:30 +0000

    support fullcontact v3 API, only supporting new person and company enrich EPs

--- fullcontact_aio/fullcontact.py
+++ fullcontact_aio/fullcontact.py
@@ -17,33 +17,20 @@ class FullContactRespoonse(object):
 class FullContact(object):
     def __init__(self, api_key):
         self.api_key = api_key
-        self.base_url = 'https://api.fullcontact.com/v2/'
-        self.get_endpoints = {
-            'person': 'person.json',
-            'company': 'company/lookup.json',
-            'company_search': 'company/search.json',
-            'disposable': 'email/disposable.json',
-            'name_normalizer': 'name/normalizer.json',
-            'name_deducer': 'name/deducer.json',
-            'name_similarity': 'name/similarity.json',
-            'name_stats': 'name/stats.json',
-            'name_parser': 'name/parser.json',
-            'address_locationNormalizer': 'address/locationNormalizer.json',
-            'address_locationEnrichment': 'address/locationEnrichment.json',
-            'account_stats': 'stats.json'
-        }
+        self.base_url = 'https://api.fullcontact.com/v3/'
         self.post_endpoints = {
-            'batch': 'batch.json'
+            'person': 'person.enrich',
+            'company': 'company.enrich'
         }
-        for endpoint in self.get_endpoints:
-            method = lambda endpoint=endpoint, **kwargs: self.api_get(endpoint, **kwargs)
+        for endpoint in self.post_endpoints:
+            method = lambda endpoint=endpoint, **kwargs: self.api_post(endpoint, **kwargs)
             setattr(self, endpoint, method)
 
-    async def api_get(self, endpoint, **kwargs):
+    async def api_post(self, endpoint, **kwargs):
         """ Makes a FullContact API call
 
-        Formats and submits a request to the specified endpoint, appending
-        any key-value pairs passed in kwargs as a url query parameter.
+        Formats and submits a request to the specified endpoint, passing
+        any key-value pairs passed in kwargs as json body parameters.
 
         Args:
             endpoint: shortname of the API endpoint to use.
@@ -62,10 +49,12 @@ class FullContact(object):
                 module.
         """
 
-        headers = {'X-FullContact-APIKey': self.api_key}
-        endpoint = self.base_url + self.get_endpoints[endpoint]
+        breakpoint()
+        headers = {'Authorization': 'Bearer {}'.format(self.api_key)}
+        headers['Content-Type'] = 'application/json'
+        endpoint = self.base_url + self.post_endpoints[endpoint]
         async with aiohttp.ClientSession() as session:
-            async with session.get(endpoint, params=kwargs, headers=headers) as request:
+            async with session.post(endpoint, json=kwargs, headers=headers) as request:
                 try:
                     rate_limit_remaining = request.headers["x-rate-limit-remaining"]
                 except KeyError:
